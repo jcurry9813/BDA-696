bb308b2acf8f1711f7d452e4763b85bdd7e59259
Jim Curry <jimcurry@Jims-MacBook-Pro.local>
2020-09-22 13:32:12 -0700

    A2 first commit

---
+++ Assignments/A2.sql
@@ -0,0 +1,62 @@
+-- BDA 696 Assignment 2
+
+-- Use baseball database to create 3 tables
+
+use baseball;
+
+-- First table: Career Batting Average
+
+drop table if exists CareerBA;
+
+create table CareerBA
+select batter, sum(Hit)/sum(atBat) as BA
+from batter_counts where atBat>0
+group by batter;
+
+select * from CareerBA;
+
+-- Second table: Seasonal Batting Average
+
+drop table if exists SeasonalBA;
+
+create table SeasonalBA
+select batter_counts.batter, sum(batter_counts.Hit)/sum(batter_counts.atBat) as BA, 
+year(game.local_date) as Season
+from batter_counts join game on batter_counts.game_id=game.game_id where atBat>0
+group by Season, batter
+order by batter, Season;
+
+select * from SeasonalBA;
+
+-- Third table: Rolling Batting Average
+
+drop table if exists RollingBA;
+
+create table RollingBA
+select Hit, atBat, batter, date(game.local_date) as gameday,
+(sum(batter_counts.Hit) over (partition by batter
+						      order by game.local_date
+						      asc rows between 101 preceding
+						      and 1 preceding))
+						/
+(sum(batter_counts.atBat) over (partition by batter
+							    order by game.local_date
+							    asc rows between 101 preceding
+							    and 1 preceding))
+as rollingavg
+from batter_counts join game on batter_counts.game_id = game.game_id where atBat > 0
+group by gameday, batter, Hit, atBat, game.local_date
+order by gameday, batter;
+
+select * from RollingBA;
+
+
+
+
+
+	
+	
+	
+
+
+
